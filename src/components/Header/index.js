80b7eb34b722e88125a5bce42c386ef8273ff00b
Leslie <leslie.passante@gmail.com>
2018-05-20 11:03:24 -0400

    ran prettier

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,12 @@ import logo from 'images/header-logo.svg';
 import './style.scss';
 
 const Header = ({ siteTitle }) => (
-  <div className='container'>
-    <div className='row'>
-      <div className='col-12'>
-        <header className='header'>
+  <div className="container">
+    <div className="row">
+      <div className="col-12">
+        <header className="header">
           <img src={logo} alt={siteTitle} />
-          <h3 className='body-2'>Coming Fall 2018</h3>
+          <h3 className="body-2">Coming Fall 2018</h3>
         </header>
       </div>
     </div>
--- src/components/SubscribeForm/index.js
+++ src/components/SubscribeForm/index.js
@@ -4,13 +4,11 @@ import MailchimpSubscribe from 'react-mailchimp-subscribe';
 const url = 'https://design.us18.list-manage.com/subscribe/post?u=84e246a772144cb0d4c59553a&amp;id=a15d1dc2f9';
 
 const SubscribeForm = () => (
-  <section className='subscribe-form'>
-
-    <div className='subscribe-form-wrapper'>
-      <input type='email' placeholder='Your email' />
-      <button></button>
+  <section className="subscribe-form">
+    <div className="subscribe-form-wrapper">
+      <input type="email" placeholder="Your email" />
+      <button />
     </div>
-
   </section>
 );
 
