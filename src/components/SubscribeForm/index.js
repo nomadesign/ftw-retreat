c1007e0b7a42ea2810bc259867131bdcc748fbf8
Leslie <leslie.passante@gmail.com>
2018-05-20 12:36:11 -0400

    adding more flexible subscription form

--- src/components/SubscribeForm/index.js
+++ src/components/SubscribeForm/index.js
@@ -1,17 +1,3 @@
-import React from 'react';
-import MailchimpSubscribe from 'react-mailchimp-subscribe';
-
-const url = 'https://design.us18.list-manage.com/subscribe/post?u=84e246a772144cb0d4c59553a&amp;id=a15d1dc2f9';
-
-const SubscribeForm = () => (
-  <section className="subscribe-form">
-    <div className="subscribe-form-wrapper">
-      <input type="email" placeholder="Your email" />
-      <button />
-    </div>
-  </section>
-);
-
-// <MailchimpSubscribe url={url} />
+import SubscribeForm from './SubscribeForm';
 
 export default SubscribeForm;
