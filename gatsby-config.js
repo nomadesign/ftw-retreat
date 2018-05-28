4b2173e72af63f3d62ad00873bdeb53b1c6a1d46
Leslie <leslie.passante@gmail.com>
2018-05-28 08:08:09 -0400

    remove whitespace

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'For the Women Retreat',
-    description: ' A Beautiful Retreat for Creative Women. Five all-inclusive days of pliable time. Coming Fall 2018.',
+    description: 'A beautiful retreat for creative women. Five all-inclusive days of pliable time. Coming Fall 2018.',
     keywords: 'women, retreat, creative, inclusive'
   },
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass']
