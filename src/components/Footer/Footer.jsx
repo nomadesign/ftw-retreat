04bc31a497ebd75b545abeefdf75f3ba6e8d5ef4
Victor Coulon <vic.coulon@gmail.com>
2018-05-28 10:31:34 -0400

    Customize privacy policy page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,11 +24,11 @@ const Footer = () => (
 
     <div className="container">
       <div className="row">
-        <div className="col-8 footer-quote">
+        <div className="col-8 ">
           <p className="body-2">“It’s very important to learn again the art of resting and relaxing.”</p>
           <p className="body-3">⏤ Thich Nhat Hanh</p>
         </div>
-        <div className="col-4 footer-contact">
+        <div className="col-4">
           <a href="mailto:contact@forthewomenretreat.com" className="media">
             <img src={iconEmail} width="24" className="media-figure" />
             <div className="media-body">contact@forthewomenretreat.com</div>
@@ -37,9 +37,9 @@ const Footer = () => (
             <img src={iconInstagram} width="24" className="media-figure" />
             <div className="media-body">@ftwretreat</div>
           </a>
-          <Link to="/privacy/" className="media">
-            <img src={iconEmail} width="24" className="media-figure" />
-            <div className="media-body">privacy policy</div>
+          <br />
+          <Link to="/privacy/" className="footer-privacy">
+            Privacy Policy
           </Link>
         </div>
       </div>
