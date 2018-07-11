31fb2776577c510c9a7fb7bf58a6ed023e4996f5
Victor Coulon <vic.coulon@gmail.com>
2018-07-10 21:55:02 -0600

    Add FAQ page

---
+++ src/pages/faq.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Footer from 'components/Footer';
+import Link from 'gatsby-link';
+
+const FaqPage = () => (
+  <div>
+    <section className="faq">
+      <div className="container">
+
+        <div className="row">
+          <div className="col-12">
+
+            <h1 className="title-1">FAQ</h1>
+
+            <h3 className="subhead-1">question</h3>
+            <p className="body-2">
+              answer
+            </p>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <Footer />
+  </div>
+);
+
+export default FaqPage;
