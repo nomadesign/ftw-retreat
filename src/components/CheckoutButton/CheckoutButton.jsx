bae70f753562df9664022b4491364ca8ae80c4f5
Leslie <leslie.passante@gmail.com>
2018-07-13 16:14:56 -0700

    fix bug with buttons not working

--- src/components/CheckoutButton/CheckoutButton.jsx
+++ src/components/CheckoutButton/CheckoutButton.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import uuid from 'uuid/v4';
 
 class CheckoutButton extends React.Component {
+  constructor(...props) {
+    super(...props);
+    this.state = { uniqueID: uuid() };
+  }
   componentDidMount() {
     const { id, showSecondaryCTA } = this.props;
+    const { uniqueID } = this.state;
 
     window.EBWidgets.createWidget({
       widgetType: 'checkout',
       eventId: id,
       modal: true,
-      modalTriggerElementId: `eventbrite-widget-modal-trigger-${id}`
+      modalTriggerElementId: `eventbrite-widget-modal-trigger-${id}-${uniqueID}`
     });
   }
   render() {
     const { id, showSecondaryCTA } = this.props;
+    const { uniqueID } = this.state;
 
     return (
       <section className="checkout-button">
-        <button id={`eventbrite-widget-modal-trigger-${id}`} className="button" type="button">
+        <button id={`eventbrite-widget-modal-trigger-${id}-${uniqueID}`} className="button" type="button">
           Reserve Your Spot
         </button>
         {showSecondaryCTA && (
           <div className="checkout-button-faq">
-            Got more questions? Check out our <Link to="/faq" className="underline">FAQ</Link>.
+            Got more questions? Check out our{' '}
+            <Link to="/faq" className="underline">
+              FAQ
+            </Link>.
           </div>
         )}
       </section>
