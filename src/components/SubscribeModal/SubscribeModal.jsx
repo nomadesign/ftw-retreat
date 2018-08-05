5684134d03c6a52f643a779dd340b5e1fed591b3
Leslie <leslie.passante@gmail.com>
2018-08-05 14:56:28 -0400

    clean up

--- src/components/SubscribeModal/SubscribeModal.jsx
+++ src/components/SubscribeModal/SubscribeModal.jsx
@@ -2,22 +2,12 @@ import React from 'react';
 import Link from 'gatsby-link';
 import SubscribeForm from 'components/SubscribeForm';
 
-/*const SubscribeModal = () => (
-  <section className="subscribe-modal-container">
-    <div className="subscribe-modal-overlay overlay"></div>
-    <div className="subscribe-modal">
-      <button>close</button>
-      <SubscribeForm />
-    </div>
-  </section>
-);*/
-
 class SubscribeModal extends React.Component {
   componentDidMount() {
-    console.log('here');
-    eval(
-      "require(['mojo/signup-forms/Loader'], L => { L.start({ baseUrl: 'mc.us18.list-manage.com', uuid: 'a1476b213c1cdb5d968fa0c54', lid: '989581153d' });});"
-    );
+    /* load mailchimp modal subscribe widget */
+    window.require(['mojo/signup-forms/Loader'], L => {
+      L.start({ baseUrl: 'mc.us18.list-manage.com', uuid: 'a1476b213c1cdb5d968fa0c54', lid: '989581153d' });
+    });
   }
   render() {
     return null;
