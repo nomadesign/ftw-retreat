e5f3e2189d970dddb39dd985247ccf659e6b733c
Leslie <leslie.passante@gmail.com>
2018-08-05 13:58:46 -0400

    add mailchimp modal

--- src/components/SubscribeModal/SubscribeModal.jsx
+++ src/components/SubscribeModal/SubscribeModal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'gatsby-link';
 import SubscribeForm from 'components/SubscribeForm';
 
-const SubscribeModal = () => (
+/*const SubscribeModal = () => (
   <section className="subscribe-modal-container">
     <div className="subscribe-modal-overlay overlay"></div>
     <div className="subscribe-modal">
@@ -10,6 +10,18 @@ const SubscribeModal = () => (
       <SubscribeForm />
     </div>
   </section>
-);
+);*/
+
+class SubscribeModal extends React.Component {
+  componentDidMount() {
+    console.log('here');
+    eval(
+      "require(['mojo/signup-forms/Loader'], L => { L.start({ baseUrl: 'mc.us18.list-manage.com', uuid: 'a1476b213c1cdb5d968fa0c54', lid: '989581153d' });});"
+    );
+  }
+  render() {
+    return null;
+  }
+}
 
 export default SubscribeModal;
