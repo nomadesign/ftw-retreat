407b49d53bdfb7991b513b2acfd01068e277bc7d
Victor Coulon <victor@helpscout.com>
2018-10-12 16:03:54 -0600

    Update Footer Design

--- src/components/modules/Footer/Footer.jsx
+++ src/components/modules/Footer/Footer.jsx
@@ -20,32 +20,36 @@ const Footer = ({ showRegistration }) => (
 
     <div className="footer-wrapper">
       <div className="container">
-        <div className="row">
-          <div className="col-4">
-            <a href="mailto:contact@forthewomenretreat.com" target="_blank" rel="noopener noreferrer" className="media footer-social">
-              <img src={iconEmail} width="24" className="media-figure" alt="email" />
-              <div className="media-body">contact@forthewomenretreat.com</div>
-            </a>
-            <a href="https://www.instagram.com/ftwretreat" target="_blank" rel="noopener noreferrer" className="media footer-social">
-              <img src={iconInstagram} width="24" className="media-figure" alt="instagram" />
-              <div className="media-body">@ftwretreat</div>
-            </a>
 
-          </div>
-          <div className="col-8 footer-quote">
-            <p className="body-2">“It’s very important to learn again the art of resting and relaxing.”</p>
-            <p className="body-3">⏤ Thich Nhat Hanh</p>
+        <div className="row">
+          <div className="col-12 center">
+            <div className="footer-quote">
+              <p className="subhead-1">“It’s very important to learn again the art of resting and relaxing.”</p>
+              <p className="body-3">⏤ Thich Nhat Hanh</p>
+            </div>
           </div>
         </div>
 
         <div className="row">
-          <div className="col-12">
+          <div className="col-12 center">
+            <h3 className="footer-instagram">
+              Follow us on Instagram{' '}
+              <a href="https://www.instagram.com/ftwretreat" target="_blank" rel="noopener noreferrer">
+                @ftwretreat
+              </a>
+            </h3>
             <InstagramFeed />
           </div>
         </div>
 
         <div className="row">
-          <div className="col-12">
+          <div className="col-6">
+            <a href="mailto:contact@forthewomenretreat.com" target="_blank" rel="noopener noreferrer" className="media footer-social">
+              <img src={iconEmail} width="24" className="media-figure" alt="email" />
+              <div className="media-body">contact@forthewomenretreat.com</div>
+            </a>
+          </div>
+          <div className="col-6 footer-align-right">
             <Link to="/privacy/" className="footer-privacy">
               Privacy Policy
             </Link>{' '}
