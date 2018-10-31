0a4e58b52625457c21a3e6d183c9c04f75b80a91
Victor Coulon <victor@helpscout.com>
2018-10-31 16:38:50 -0600

    Update src/pages/book.js

---
+++ src/pages/book.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Shell from 'layouts/Shell';
+import Footer from 'modules/Footer';
+
+const Book = () => (
+  <Shell>
+    <section className="book">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+
+          BUY BUTTONS HERE
+          SEE https://codepen.io/Victa/pen/mzNPQe for code
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <Footer showRegistration="true" />
+  </Shell>
+);
+
+export default Book;
